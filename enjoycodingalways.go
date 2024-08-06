ab257d1bb965bc862ec9a431e837504ad1741bf3
mikevidotto <mikevidotto@live.com>
2024-08-05 22:58:16 -0400

    worked on frontend

--- enjoycodingalways.go
+++ enjoycodingalways.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	//need server
+	directory := http.Dir("./static/")
+	fs := http.FileServer(directory)
+	http.Handle("/", fs)
+
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatal("Error with ListenAndServe:", err)
+	}
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleProjects(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleDoc(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+
 }
